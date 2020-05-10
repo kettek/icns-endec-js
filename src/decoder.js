d38ee01cdd67e395ae265b81534c18411a0fa3aa
kts of kettek (nyaa) <kettek1@kettek.net>
2020-05-10 05:52:17 -0700

    Add comment for how we'll decode

--- src/decoder.js
+++ src/decoder.js
@@ -20,6 +20,15 @@ class Decoder {
    * @returns [IconEntries]
    */
   decode() {
+    // Read 0,4 for "icns"
+    // Read 4,4 for file length in bytes (msb first)
+    // Start read loop
+    // Read 0,4 Icon type
+    // For now, just icp4,icp5,icp6,ic07,ic08,ic09,ic10,ic11,ic12,ic13,ic14 as these are JPEG 2000 or PNG.
+    // Probably also just for 'name', 'TOC ', 'icnV', 'info'
+    // Read 4,4 Length of icon in bytes, including type and length, msb
+    // Read 8,(length-8)
+    // End read loop
   }
 }
 
